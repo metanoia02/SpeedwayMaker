97498bd31677dbad7037462f699b4ad2d279b61c
Peter Fry <zw301808@ou.ac.uk>
2023-06-22 23:52:48 +0100

    Added team images to programme

--- src/Pages/Programme.jsx
+++ src/Pages/Programme.jsx
@@ -1,34 +1,41 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import SpeedwayApi from "../api";
-import data from '../data.json';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import SpeedwayApi from '../api';
+import data from '../data';
 
-import { HeatHeading } from "../Components/HeatHeading";
-import { HeatRow } from "../Components/HeatRow";
-import { RiderHeading } from "../Components/RiderHeading";
-import { RiderRow } from "../Components/RiderRow";
+import { HeatHeading } from '../Components/HeatHeading';
+import { HeatRow } from '../Components/HeatRow';
+import { RiderHeading } from '../Components/RiderHeading';
+import { RiderRow } from '../Components/RiderRow';
 
-function padRiders(array) {
-  if (array.length < 8) {
-    while(array.length < 8) {
-    array.push({
-      position: array.length + 1,
-      rider: {
-        firstName: "",
-        lastName: "",
-      }
-    })
+/**
+ * Fills the array with blank riders to a length of 8
+ * @param {TeamRiderDTO[]} teamRiders
+ */
+function padRiders(teamRiders) {
+  if (teamRiders.length < 8) {
+    while (teamRiders.length < 8) {
+      teamRiders.push({
+        position: teamRiders.length + 1,
+        rider: {
+          firstName: '',
+          lastName: '',
+        },
+      });
+    }
   }
-  }
-};
+}
 
+/**
+ * A Speedway score sheet. Blank if no team inputs.
+ */
 function Programme() {
   const location = useLocation();
 
   let homeId = undefined;
   let awayId = undefined;
 
-  if(location.state) {
+  if (location.state) {
     homeId = location.state[0].value;
     awayId = location.state[1].value;
   }
@@ -40,23 +47,25 @@ function Programme() {
 
   useEffect(() => {
     async function fetchData() {
-      const result = await SpeedwayApi.get("/TeamRider");
+      const result = await SpeedwayApi.get('/TeamRider');
 
       let home = [];
       let away = [];
 
+      // Make sorted lists of home and away riders.
       home = result.data
         .filter((rider) => rider.teamId == homeId)
         .sort((a, b) => a.position - b.position);
-
-      setHomeTeamRiders(home);
-      padRiders(home);
 
       away = result.data
         .filter((rider) => rider.teamId == awayId)
         .sort((a, b) => a.position - b.position);
 
+      setHomeTeamRiders(home);
       setAwayTeamRiders(away);
+
+      // Pad the array to add extra line required. Also fills blank form if no team input.
+      padRiders(home);
       padRiders(away);
 
       const heats = [];
@@ -64,6 +73,7 @@ function Programme() {
       let isHome = true;
       let colourIndex = 0;
 
+      // Fill 16 heats with 4 riders
       for (let i = 0; i < 64; i++) {
         let rider;
 
@@ -71,7 +81,7 @@ function Programme() {
 
         heats[i] = {
           useSpan: i % 2 == 0,
-          heat: i % 4 == 0 ? String((heatNumber += 1)) : "",
+          heat: i % 4 == 0 ? String((heatNumber += 1)) : '',
           gateL: data.gateL[i],
           gateR: data.gateR[i],
           riderNo: data.riderOrder[i],
@@ -84,14 +94,13 @@ function Programme() {
           rider = away.filter((rider) => rider.position == heats[i].riderNo);
         }
 
-        if(!rider) {
-          rider.riderName = "";
+        // TODO: Rider name logic needs to be moved to the api.
+        if (!rider) {
+          rider.riderName = '';
         } else if (rider.length == 1) {
           heats[i].riderName =
-            (rider[0].rider.nickname
-              ? rider[0].rider.nickname
-              : rider[0].rider.firstName) +
-            " " +
+            (rider[0].rider.nickname ? rider[0].rider.nickname : rider[0].rider.firstName) +
+            ' ' +
             rider[0].rider.lastName;
         }
 
@@ -99,6 +108,7 @@ function Programme() {
         if (i % 2 == 1) isHome = !isHome;
       }
 
+      // Split heats into two arrays for one component in each column.
       setHeats1(heats.slice(0, 32));
       setHeats2(heats.slice(32, 64));
     }
@@ -106,53 +116,65 @@ function Programme() {
   }, []);
 
   return (
-    <div className="programme">
-      <div className="row">
-        <div className="column">
-          <table>
-            <thead>
-              <RiderHeading />
-            </thead>
-            <tbody>
-              {homeTeamRiders.map((rider, index) => (
-                <RiderRow key={index} {...rider} />
-              ))}
-            </tbody>
-          </table>
-          <br />
-          <table>
-            <thead>
-              <HeatHeading />
-            </thead>
-            <tbody>
-              {heats1.map((heat, index) => (
-                <HeatRow key={index} {...heat} />
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="column">
-          <table>
-            <thead>
-              <RiderHeading />
-            </thead>
-            <tbody>
-              {awayTeamRiders.map((rider, index) => (
-                <RiderRow key={index} {...rider} />
-              ))}
-            </tbody>
-          </table>
-          <br />
-          <table>
-            <thead>
-              <HeatHeading />
-            </thead>
-            <tbody>
-              {heats2.map((heat, index) => (
-                <HeatRow key={index} {...heat} />
-              ))}
-            </tbody>
-          </table>
+    <div className='container d-print-inline'>
+      <div className='programme table-responsive'>
+        <div className='row'>
+          <div className='column'>
+            <div className='riders '>
+              <table className='centred'>
+                <thead>
+                  <RiderHeading />
+                </thead>
+                <tbody>
+                  {homeTeamRiders.map((rider, index) => (
+                    <RiderRow key={index} {...rider} />
+                  ))}
+                </tbody>
+              </table>
+              <img src={'./teams/' + homeId + '.png'} />
+            </div>
+            <br />
+            <div className='heats'>
+              <table className='centred'>
+                <thead>
+                  <HeatHeading />
+                </thead>
+                <tbody>
+                  {heats1.map((heat, index) => (
+                    <HeatRow key={index} {...heat} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='riders'>
+              <table className='centred'>
+                <thead>
+                  <RiderHeading />
+                </thead>
+                <tbody>
+                  {awayTeamRiders.map((rider, index) => (
+                    <RiderRow key={index} {...rider} />
+                  ))}
+                </tbody>
+              </table>
+              <img src={'./teams/' + awayId + '.png'} />
+            </div>
+            <br />
+            <div className='heats'>
+              <table className='centred'>
+                <thead>
+                  <HeatHeading />
+                </thead>
+                <tbody>
+                  {heats2.map((heat, index) => (
+                    <HeatRow key={index} {...heat} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
