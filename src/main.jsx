3c97fb69fa8afb40cf0ca57a9503a9a4dc4c2d9b
metanoia02 <metanoia3902@gmail.com>
2023-06-27 16:34:23 +0100

    Restyling code

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from './Pages/App.jsx'
-import Programme from './Pages/Programme.jsx'
-import Layout from './Pages/Layout.jsx'
+// Styles
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Pages
+import App from './Pages/App.jsx';
+import Programme from './Pages/Programme.jsx';
+import Layout from './Pages/Layout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<App />} />
-                    <Route path="programme" element={<Programme />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path='programme' element={<Programme />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
