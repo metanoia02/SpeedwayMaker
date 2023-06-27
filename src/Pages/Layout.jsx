ad9a808e38d4a9a4bb58cd5692db61e70262d350
metanoia02 <metanoia3902@gmail.com>
2023-06-27 16:34:32 +0100

    Fixed print refresh page bug

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,71 +1,60 @@
-import { Outlet, Link } from 'react-router-dom'
-import React, { useState } from 'react'
+import { Outlet, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Layout = () => {
-    const [isNavCollapsed, setIsNavCollapsed] = useState(true)
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed)
-    const handlePrint = () => window.print()
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  function handlePrint(e) {
+    e.preventDefault();
+    window.print();
+  }
 
-    return (
-        <>
-            <nav
-                className="navbar navbar-dark bg-dark navbar-expand-lg d-print-none sticky-top"
-                style={{ paddingLeft: '1rem' }}
-            >
-                <a
-                    className="navbar-brand text-info font-weight-bolder"
-                    href="/"
-                >
-                    <span className="d-print-none">
-                        Speedway Programme Maker
-                    </span>
-                </a>
-                <button
-                    className="custom-toggler navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded={!isNavCollapsed ? true : false}
-                    aria-label="Toggle navigation"
-                    onClick={handleNavCollapse}
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <>
+      <nav
+        className='navbar navbar-dark bg-dark navbar-expand-lg d-print-none sticky-top'
+        style={{ paddingLeft: '1rem' }}
+      >
+        <Link className='navbar-brand text-info font-weight-bolder' to='/'>
+          <span className='d-print-none'>Speedway Programme Maker</span>
+        </Link>
+        <button
+          className='custom-toggler navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label='Toggle navigation'
+          onClick={handleNavCollapse}
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
 
-                <div
-                    className={`${
-                        isNavCollapsed ? 'collapse' : ''
-                    } navbar-collapse`}
-                    id="navbarNav"
-                >
-                    <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/programme">
-                                Programme
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                href="#"
-                                onClick={handlePrint}
-                            >
-                                Print
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item active'>
+              <a className='nav-link' href='/'>
+                Home
+              </a>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='programme'>
+                Programme
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' onClick={handlePrint}>
+                Print
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-            <Outlet />
-        </>
-    )
-}
+      <Outlet />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
